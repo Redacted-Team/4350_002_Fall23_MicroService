e8e78fc619341ab9a25ae320a2214dba4730e1fc
Patrick Vergason <VERGASON@ETSU.EDU>
2024-04-08 20:51:19 -0400

    Added comments to the logging files and moved them into their own folder

--- GameMicroServer/Program.cs
+++ GameMicroServer/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// This creates the services for the Logging files.
 builder.Services.AddLogging(builder =>
 {
     builder.AddConsole();
